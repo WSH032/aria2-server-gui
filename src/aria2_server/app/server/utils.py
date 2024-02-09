14f90bcbb4beab623a3a738e9165a1e0ebcffe78
WSH032 <614337162@qq.com>
2024-02-09 17:21:29 +0800

    feat: add `ServerConfig` type in `app.server.utils`

--- src/aria2_server/app/server/utils.py
+++ src/aria2_server/app/server/utils.py
@@ -1,3 +1,9 @@
-from aria2_server.app._core.server_config import get_server_config_in_db
+from aria2_server.app._core.server_config import (
+    ServerConfig,
+    get_server_config_in_db,
+)
 
-__all__ = ("get_server_config_in_db",)
+__all__ = (
+    "ServerConfig",
+    "get_server_config_in_db",
+)
